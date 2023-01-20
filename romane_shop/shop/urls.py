aa8f4d4bea9c44c0c0c1db90d5ddbda24aa2c164
Pierre Congiu <pierre.congiu@estaca.eu>
2023-01-20 13:36:52 +0700

    create views, create urls.py and add view directories

---
+++ romane_shop/shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('gallery_<str:category>/', views.gallery, name='gallery'),
+    # path('gallery/<str:category>', views.gallery, name='gallery'),
+    path('gallery_<str:category>/<str:product>', views.product_page, name='product_page'),
+    path('<str:firstname>_<str:lastname>/', views.customer_space, name='customer_space'),
+]
--- romane_shop/shop/views.py
+++ romane_shop/shop/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Product, Customer, Command
 
 # Create your views here.
+def home(request):
+    template = loader.get_template("home.html")
+    return HttpResponse(template.render())
+
+def gallery(request, category):
+    products = Product.objects.get(category=category)
+    template = loader.get_template("gallery.html")
+    context = {
+        'products': products,
+    }
+    return HttpResponse(template.render(context, request))
+
+def product_page(request, product_name):
+    product = Product.objects.get(product_name=product_name)
+    template = loader.get_template("product_page.html")
+    context = {
+        'product': product,
+    }
+    return HttpResponse(template.render(context, request))
+
+def customer_space(request, email):
+    customer = Customer.objects.get(email=email)
+    command = Command.objects.get(customer_email=email)
+    template = loader.get_template("customer_space.html")
+    context = {
+        'customer': customer,
+        'command': command,
+    }
+    return HttpResponse(template.render(context, request))
