6839766a7c5ee590e253f6b3e35d4ba40b5c2843
Pierre Congiu <pierre.congiu@estaca.eu>
2023-01-20 12:01:14 +0700

    add shop to installed_apps in settings, edit models to create our data tables and make migrations

--- romane_shop/shop/models.py
+++ romane_shop/shop/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    image_name = models.CharField(max_length=255)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    description = models.TextField()
+    size = models.CharField(max_length=255)
+    weight = models.FloatField()
+    color = models.CharField(max_length=255)
+    material = models.CharField(max_length=255)
+
+class Customer(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    address_street = models.CharField(max_length=255)
+    address_zipcode = models.CharField(max_length=255)
+    address_city = models.CharField(max_length=255)
+    address_contry = models.CharField(max_length=255)
+
+class Command(models.Model):
+    command_number = models.IntegerField()
+    items = models.CharField(max_length=255)
+    payed = models.BooleanField(default=False)
+    customer_email = models.EmailField()
